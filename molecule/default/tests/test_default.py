bf5634161f4f6cbafd2770a563ab1c9d47cf38b1
Stan Chan <stanchan@gmail.com>
2018-02-14 09:06:55 -0800

    Clean up values

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,8 @@
-import os
+# Molecule managed
 
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
-
-
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+---
+file:
+  /etc/hosts:
+    exists: true
+    owner: root
+    group: root
